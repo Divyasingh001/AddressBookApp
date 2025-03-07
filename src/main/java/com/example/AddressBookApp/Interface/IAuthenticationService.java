94247162a59a3f5d274327e68030a13af01da2c8
Divyasingh001 <divyashunk2003@gmail.com>
2025-03-07 12:17:09 +0530

    Register

--- src/main/java/com/example/AddressBookApp/Interface/IAuthenticationService.java
+++ src/main/java/com/example/AddressBookApp/Interface/IAuthenticationService.java
@@ -3,7 +3,6 @@ package com.example.AddressBookApp.Interface;
 
 import com.example.AddressBookApp.DTO.AuthUserDTO;
 import com.example.AddressBookApp.model.AuthUser;
-
 public interface IAuthenticationService {
     AuthUser register(AuthUserDTO userDTO) throws Exception;
 
